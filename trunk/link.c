0147ccab929d7a740fec97e5f2f7124ac2a01647
raph.byrne@gmail.com <raph.byrne@gmail.com@1d888a2a-1c82-1147-5847-4a6a7c7edce4>
2011-05-14 04:24:01 +0000

    Unfinished dry coding of link layer

--- trunk/link.c
+++ trunk/link.c
@@ -10,8 +10,15 @@
 
 /* TODO: define a frame structure */
 
+#define WAITINGTIME 
+
 static bool attemptingToSend = false;
 char* nextMsg;
+
+CnetTimerID timer;
+CnetTimerID sendTimer;
+
+FRAME* fBuf;
 
 /* returns max # of bytes of data that link_send_data or link_send_info
  * will currently accept */
@@ -23,19 +30,16 @@ void send_frame(FRAMETYPE type, CnetAddr dest, int len, char *data) {
 	f.dest = dest;
 	f.src = nodeinfo.nodenumber;
 	f.len = len;
-	//probably won't need seq or msg
+	memcpy(f.msg, data, len);
 	
 	//send frame over cnet
+	CHECK(CNET_write_physical(1, &f, &len));
 }
 
 /* send data msg of length len to receiver recv 
  */
 void link_send_data( char * msg, int len, CnetAddr recv) {
-	FRAME f;
-	//send RTS
-	//wait for CTS
-	//send data
-	//wait for ack
+	//put a frame into the frame buffer
 }
 
 /* send info msg of length len to receiver recv
@@ -46,12 +50,32 @@ void link_send_data( char * msg, int len, CnetAddr recv) {
  * the oracle
  */
 void link_send_info( char * msg, int len, CnetAddr recv) {
+	//send a beacon frame
 }
 
-/* called on program initialisation */
-void link_init() {
-	/* set mac address */
-	/* register CNET handlers for physical ready */
+static EVENT_HANDLER(collision) {
+	//binary exponential backoff
+}
+
+static EVENT_HANDLER(send) {
+	if(CNET_carrier_sense(1)==0 /*&& we have something to send*/) {
+		//send RTS
+		//wait for CTS
+		//send data
+		//wait for ack
+	}
+}
+
+static EVENT_HANDLER(timeout) {
+	FRAMETYPE type;
+	if(CNET_timer_data(timer, &type) == 0) {
+		switch(type) {
+			case DL_RTS:
+				sendTimer = CNET_start_timer(EV_TIMER2, 0, 0);
+				break;
+			
+		}
+	}
 }
 
 static EVENT_HANDLER(recieve) {
@@ -60,22 +84,53 @@ static EVENT_HANDLER(recieve) {
 	int link;
 	
 	//recieve the frame
+	CHECK(CNET_read_physical(1, &f, &len));
 	
 	switch(f.kind) {
+		case DL_BEACON:
+			oracle_recv(&f.msg, len, f.src);
+			break;
 		case DL_RTS:
-			send_frame(DL_CTS, f.src, f.len, NULL);
+			if(f.dest == nodeinfo.nodenumber) {
+				send_frame(DL_CTS, f.src, len, NULL);	
+			}
+			FRAMETYPE type = DL_RTS;
+			timer = CNET_start_timer(EV_TIMER1, (2*FRAME_HEADER_SIZE + len)/linkinfo.bandwidth + linkinfo.propegationdelay
+				, &type);
+			break;
 		case DL_CTS:
-			if(attemptingToSend)
-				send_frame(DL_DATA, f.src, f.len, nextMsg);
+			if(f.dest == nodeinfo.nodenumber) {
+				send_frame(DL_DATA, f.src, len, nextMsg);
+			}
+			FRAMETYPE type = DL_CTS;
+			timer = CNET_start_timer(EV_TIMER1, (FRAME_HEADER_SIZE + len)/linkinfo.bandwidth + linkinfo.propegationdelay, &type);
+			break;
 			//otherwise wait
 		case DL_DATA:
 			if(f.dest == nodeinfo.nodenumber) {
-				net_recv(f.msg, f.len, f.src);
+				net_recv(f.msg, len, f.src);
 			}
+			FRAMETYPE type = DL_DATA;
+			timer = CNET_start_timer(EV_TIMER1, (FRAME_HEADER_SIZE + len)/linkinfo.bandwidth + linkinfo.propegationdelay, &type);
+			break;
 		case DL_ACK:
-			if(attemptingToSend)
+			if(f.dest == nodeinfo.nodenumber) {
+				//do something
+			}
+			CNET_stop_timer(timer);
+			break;
 				//schedule next message (or set some state that means we can send)
 	}
+}
+
+/* called on program initialisation */
+void link_init() {
+	/* set mac address */
+	/* register CNET handlers for physical ready */
+	CHECK(CNET_set_handler(EV_PHYSICALREADY, recieve, 0));
+	CHECK(CNET_set_handler(EV_TIMER1, timeout, 0));
+	CHECK(CNET_set_handler(EV_TIMER2, send, 0));
+	CHECK(CNET_set_handler(EV_FRAMECOLLISION, collision, 0);
 }
 
 /** RECEIVED DATA **/
